99196dd23d6efa156fb5fc1f739750c7b5667653
Luis Rosales <l.rosales@inpsyde.com>
2024-03-15 00:06:24 +0100

    ignorando __pycache__ y agregando modelo Blog

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+   titulo = models.CharField(max_length=150)
+   subtitulo = models.CharField(max_length=150)
+   cuerpo = models.TextField(blank=True,default="", null=True)
+   fecha = models.DateTimeField()
+   author = models.CharField(max_length=150)
+   created_at = models.DateTimeField(auto_now_add=True)
+   modified_at = models.DateTimeField(auto_now=True)
